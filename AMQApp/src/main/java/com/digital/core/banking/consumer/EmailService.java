e3595ddb7e220f978720e2fb3bc61b2ade571407
sathishvt <sathishvt@VinsLaptop>
2020-05-10 08:04:01 +0530

    Added Email Support

--- AMQApp/src/main/java/com/digital/core/banking/consumer/EmailService.java
+++ AMQApp/src/main/java/com/digital/core/banking/consumer/EmailService.java
@@ -4,6 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+import java.io.IOException;
+
 @Component
 public class EmailService {
 
@@ -12,7 +22,27 @@ public class EmailService {
 	public void sendEmail(String message) {
 
 		log.info("--------------------------------");
-		log.info("Receive message in bean '{}' from queue.", message);
+
+		Email from = new Email("admin@gmail.com");
+		String subject = "Sending with Twilio SendGrid is Fun";
+		Email to = new Email("satishkumar.pk@gmail.com");
+		Content content = new Content("text/plain", message);
+		Mail mail = new Mail(from, subject, to, content);
+
+		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		Request request = new Request();
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			System.out.println(response.getStatusCode());
+			System.out.println(response.getBody());
+			System.out.println(response.getHeaders());
+		} catch (IOException ex) {
+			log.error("Error in sending email:" + ex.getMessage());
+		}
+
 		log.info("**********************************");
 	}
 }
